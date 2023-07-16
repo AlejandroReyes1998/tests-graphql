740fea1204e57c2dae62978ac748eed9f24b925d
Alejandro Reyes <areyesv11@gmail.com>
2023-07-15 19:01:16 -0600

    Hola Mundo

---
+++ helloWorld.js
@@ -0,0 +1,24 @@
+const { graphql, buildSchema } = require("graphql")
+
+// Construcción del Schema con base a notación de Graphql
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+// Función para definir un endpoint definitivo hacia donde van a llegar todas las consultas
+const rootValue = {
+  hello: () => {
+    return "Hello world!"
+  },
+}
+
+//Se manda a llamar el programa con el objeto que definimos previamente
+graphql({
+  schema,
+  source: "{ hello }",
+  rootValue,
+}).then(response => {
+  console.log(JSON.stringify(response))
+})
